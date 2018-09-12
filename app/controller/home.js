c00a98bfd74572ef6e727aeeb4e3e0f3e2d1a396
yaoleia <yaoleia@163.com>
2018-09-12 13:27:17 +0800

    Initial commit

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,24 +1,22 @@
-const Model = require('../mocks/article/list');
-
 module.exports = app => {
 
   return class HomeController extends app.Controller {
 
     async index() {
       const { ctx } = this;
-      await ctx.render('home/index.js', Model.getPage(1, 10));
+      await ctx.render('home/index.js');
     }
 
     async client() {
       const { ctx } = this;
-      await ctx.renderClient('home/index.js', Model.getPage(1, 10));
+      await ctx.renderClient('home/index.js');
     }
 
     async pager() {
       const { ctx } = this;
       const pageIndex = ctx.query.pageIndex;
       const pageSize = ctx.query.pageSize;
-      ctx.body = Model.getPage(pageIndex, pageSize);
+      ctx.body = 123;
     }
 
   };
