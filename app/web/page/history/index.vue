ded5233b191961e95181bfc8e829ccec43aae2e6
yaoleia <yaoleia@163.com>
2018-09-13 12:18:22 +0800

    request

--- app/web/page/history/index.vue
+++ app/web/page/history/index.vue
@@ -1,58 +1,30 @@
 <template>
-  <Layout description="vue server side render">
-  </Layout>
+  <layout-home>
+    <transition name="fade" mode="out-in">
+      <!-- <router-view></router-view> -->
+    </transition>
+  </layout-home>
 </template>
-<style>
-  @import "index.css";
-</style>
 <script type="text/babel">
-  import { formatDate } from 'framework/utils/utils.js';
-  export default {
-    components: {
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import { sync } from 'vuex-router-sync';
+import 'element-ui/lib/theme-chalk/index.css';
+// import createStore from './store/app';
+// import createRouter from './router';
 
-    },
-    data(){
-      return {
-        isFinish: false,
-        isLoading : false,
-        pageIndex: 1,
-        pageSize: 10
-      }
-    },
-    computed: {
-      lists(){
-        return this.list;
-      }
-    },
-    methods: {
-      fetch(){
-        this.$http.get(`${location.origin}/pager?pageIndex=${this.pageIndex}&pageSize=${this.pageSize}`).then(res=> {
-          console.log('res', res);
-          if(res.data.list && res.data.list.length){
-            this.total = res.data.total;
-            this.list = this.list.concat(res.data.list);
-          }else{
-            this.isFinish = true;
-          }
-          this.isLoading = false;
-        });
-      },
-      loadPage(){
-        if (!this.isFinish && !this.isLoading) {
-          this.isLoading = true;
-          this.pageIndex++;
-          setTimeout(()=>{
-            this.fetch();
-          }, 1500);
-        }
-      }
-    },
-    mounted() {
-      formatDate(new Date());
-      window.addEventListener('scroll', ()=>{
-        this.loadPage();
-      }, false);
-    }
-  }
+Vue.use(ElementUI);
+
+// const store = createStore();
+// const router = createRouter();
+// sync(store, router);
+
+export default {
+//   router,
+//   store,
+  components: {
+  },
+  computed: {},
+  mounted() {}
+};
 </script>
-
