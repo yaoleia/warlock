c29d7bed5526b21d0de9471af15d756753dcb1e5
yaoleia <yaoleia@163.com>
2018-09-27 15:43:20 +0800

    env

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -9,17 +9,17 @@ module.exports = app => {
         this.socket,
         'ak'
       );
-      this.socket.ioClient = setInterval(() => {
-        this.socket.emit('msg', {
-          dm_code: 'FJW5675789734WTG',
-          seg_img_path: '/img/1.jpg',
-          mask_img_path: '/img/3.jpg',
-          reg_img_path: 'http://127.0.0.1:7001/img/2.jpg',
-          dm_code_path: 'http://127.0.0.1:7001/img/dmcode--1538018674.jpg',
-          defect_type: 0,
-          timestamp: new Date().getTime()
-        });
-      }, 3000);
+      // this.socket.ioClient = setInterval(() => {
+      //   this.socket.emit('msg', {
+      //     dm_code: 'FJW5675789734WTG',
+      //     seg_img_path: '/img/1.jpg',
+      //     mask_img_path: '/img/3.jpg',
+      //     reg_img_path: 'http://127.0.0.1:7001/img/2.jpg',
+      //     dm_code_path: 'http://127.0.0.1:7001/img/dmcode--1538018674.jpg',
+      //     defect_type: 0,
+      //     timestamp: new Date().getTime()
+      //   });
+      // }, 3000);
     }
 
     this.socket.emit('res', `Hi! I've got your message: ${message}`);
