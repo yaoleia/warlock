d7c92e72d85a21b6e61cb3d56652e72ac0fa22de
yaoleia <yaoleia@163.com>
2018-11-09 11:59:40 +0800

    design page

---
+++ app/web/page/home/view/DesignPage/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        design
+    </div>
+</template>
+<style>
+</style>
+<script type="babel">
+
+    export default {
+        components: {
+        },
+        data() {
+            return {
+
+            };
+        },
+        computed: {},
+        methods: {
+        },
+        mounted() {
+        }
+    };
+</script>
