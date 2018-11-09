19367b8ddb6858f1b2199652ce6bd8b44efc0c1e
yaoleia <yaoleia@163.com>
2018-11-09 10:50:55 +0800

    g6 pages

---
+++ app/web/component/ModelFlowEditor/page.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="page" />
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                name: 'page'
+            };
+        }
+    };
+</script>
+<style lang="scss">
+    #page {
+    	margin-left: 200px;
+    	margin-right: 200px;
+    	canvas {
+    		display: block;
+    	}
+    }
+</style>
